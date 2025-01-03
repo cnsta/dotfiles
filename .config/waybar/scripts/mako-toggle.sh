a26fa5a753795f806110c06427fc2b0b4d89ccb7
cnst <adam@cnst.dev>
2025-01-03 14:50:10 +0100

    test encypt

--- .config/waybar/scripts/mako-toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Toggle Mako mode between "do-not-disturb" and "default"
-if makoctl mode | grep -q "default"; then
-    makoctl set-mode do-not-disturb
-else
-    makoctl set-mode default
-fi
